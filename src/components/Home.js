999b13700b4f504f19b86e99a7a7b04ffad35c14
pooulad <poooooladi@gmail.com>
2021-06-30 06:55:51 +0430

    home component Completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from 'react';
+import "./Home.css";
+import { Link } from "react-router-dom";
+import mylogo from "./pooulad.png"
 
 function Home() {
     return (
-        <div>
-            
+        <div className="home">
+            <div className="header_logo">
+                <img src={mylogo} alt="logo" />
+            </div>
+            <div className="main">
+                <h1>HomePage</h1>
+            </div>
+            <div className="btns">
+                <Link to="/login">
+                    <button className="loginBtn">Login</button>
+                </Link>
+                <Link to="/register">
+                    <button className="registerBtn">Register</button>
+                </Link>
+            </div>
         </div>
     )
 }
