602379fe48303630b0987e730716a3917ff065dc
pooulad <poooooladi@gmail.com>
2021-06-30 06:19:05 +0430

    App component Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from "./Register";
+import Login from "./Login";
+
+
+
 
 function App() {
   return (
-    <div className="app">
-      
-    </div>
-  )
+    <Router>
+      <div className="App">
+          <Switch>
+            <Route path="/about">
+              <Register />
+            </Route>
+            <Route path="/users">
+              <Login />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App
